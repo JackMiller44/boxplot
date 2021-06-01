61402681a03b5ff823242ebff60de5302eb2a10e
JackMiller44 <71836331+JackMiller44@users.noreply.github.com>
2021-06-01 10:46:19 -0400

    Update boxplot_styling.js

--- boxplot_styling.js
+++ boxplot_styling.js
@@ -1,1 +1,46 @@
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Custom Widget Text</legend>
+				<table>
+					<tr>
+						<td>Text</td>
+						<td><input id="backgroundColor" type="string"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+	`;
 
+	class BoxPlot_Styling extends HTMLElement {
+		  constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
+
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							backgroundColor: this.backgroundColor
+						}
+					}
+			}));
+		}
+
+		set backgroundColor(color) {
+			this._shadowRoot.getElementById("backgroundColor").value = color;
+		}
+
+		get backgroundColor() {
+			return this._shadowRoot.getElementById("backgroundColor").value;
+		}
+	}
+
+customElements.define("boxplot_styling", BoxPlot_Styling);
+})();
