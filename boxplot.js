dd21ccbab245cf1d0851b87feff13a5310a6f344
Matthew Piriya <mvp223@lehigh.edu>
2021-06-01 10:32:21 -0400

    implemented some methods

--- boxplot.js
+++ boxplot.js
@@ -1,1 +1,23 @@
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML = `
+	
+	`; //Put border radius, other styling stuff here
 
+	class BoxPlot extends HTMLElement {
+		constructor() {
+			super();
+			
+		}
+
+		addValue(val) {
+			this._values.push(val);
+		}
+
+		removeSegment(val) {
+			if(this._values.indexOf(val) !== -1) {
+				this._values.splice(this._values.indexOf(val), 1);
+			}
+		}
+	}
+})
