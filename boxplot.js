6ad8659ddd16812ad84b99e00a57595a5aee420d
mpiriya <piriya.matt@gmail.com>
2021-06-02 10:11:34 -0400

    add/remove points, update plot

--- boxplot.js
+++ boxplot.js
@@ -5,18 +5,16 @@
 		data = [];
 		constructor() {
 			super();
-			console.log("abcd");
 			this.data.x = "Data";
 			this.recalculate();
 			//TODO: understand what shadow DOMs do and how they can benefit here
 		}
 
 		conectedCallback() {
-			//TODO: figure out how to update the boxplot HTML after we add values
 			
 		}
 
-		get getvalues() {
+		get values() {
 			return this._values;
 		}
 
@@ -25,7 +23,7 @@
 			this.recalculate();
 		}
 
-		removeSegment(val) {
+		removeValue(val) {
 			if(this._values.indexOf(val) !== -1) {
 				this._values.splice(this._values.indexOf(val), 1);
 				this.recalculate();
@@ -43,7 +41,10 @@
 		}
 		
 		recalculate() {
+			this._values.sort(function(a, b) {return a-b});
+
 			const toAdd = {low: 0, q1: 0, median: 0, q3: 0, high: 0};
+
 			toAdd.low = Math.min.apply(Math, this._values);
 			toAdd.q1 = this.getMedian(this._values.slice(0, this._values.length / 2));
 			toAdd.median = this.getMedian(this._values);
@@ -54,10 +55,46 @@
 				toAdd.q3 = this.getMedian(this._values.slice(this._values.length / 2 + 1));
 			}
 			toAdd.high = Math.max.apply(Math, this._values);
+
+			this.data.pop();
 			this.data.push(toAdd);
+			console.log("recalculated");
 		}
 
 	}
 
 	customElements.define('boxplot-practice', BoxPlot);
 })();
+
+const box = document.querySelector('#boxplot');
+
+anychart.onDocumentReady(function () {
+	chart = anychart.box();
+
+	// create a box series and set the data
+	series = chart.box(box.data);
+
+	console.log(series.id());
+
+	// set the chart title
+	var title = chart.title("nice boxplot");
+
+	// set the containder id
+	chart.container("boxplot");
+
+	// initiate drawing the chart
+	chart.draw();
+});
+
+function addData() {
+	var toAdd = document.getElementById("addPoint");
+	box.addValue(parseInt(toAdd.value));
+	chart.removeSeriesAt(0);
+	series = chart.box(box.data);
+}
+
+function removeData() {
+	box.removeValue(parseInt(document.getElementById("removePoint").value));
+	chart.removeSeriesAt(0);
+	series = chart.box(box.data);
+}
