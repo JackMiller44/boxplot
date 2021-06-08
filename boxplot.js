77295d6f16e93e1f9722b66366ec8e4ec9d89912
mpiriya <piriya.matt@gmail.com>
2021-06-08 10:52:19 -0400

    can add points to graphs not at index 0

--- boxplot.js
+++ boxplot.js
@@ -28,17 +28,20 @@
 			this._showArms = newval;
 		}
 
+		//adds an additional boxplot on the current chart
 		addPlot(arr, name) {
 			const index = this._values.push(arr) - 1;
 			this.data.push({x: name});
 			this.recalculate(index);
 		}
 
+		//adds value to the dataset of boxplot at index index
 		addValue(val, index) {
 			this._values[index].push(val);
 			this.recalculate(index);
 		}
 
+		//removes value from the dataset of boxplot at index index
 		removeValue(val, index) {
 			var dataset = this._values[index];
 			if(dataset.indexOf(val) !== -1) {
@@ -47,6 +50,7 @@
 			}
 		}
 
+		//helper method for recalculate to get the median of array arr
 		getMedian(arr) {
 			if(arr.length % 2 === 0) {
 				const val1 = arr[arr.length / 2];
@@ -57,6 +61,7 @@
 			}
 		}
 		
+		//calculates the 5 key points of the boxplot at index index, and updates the data to reflect that
 		recalculate(index) {
 			var dataset = this._values[index];
 
@@ -79,12 +84,12 @@
 				toAdd.q3 = this.getMedian(dataset.slice(dataset.length / 2 + 1));
 			}
 
-			if(this._outliers) {
+			if(this._outliers) { //check if outliers is true
 				const iqr = (toAdd.q3 - toAdd.q1) * 1.5;
-				if (!(toAdd.high <= toAdd.q3+iqr)){
+				if (toAdd.high > toAdd.q3+iqr){
 					toAdd.high = toAdd.q3+iqr;
 				}
-				if (!(toAdd.low >= toAdd.q1-iqr)){
+				if (toAdd.low < toAdd.q1-iqr){
 					toAdd.low = toAdd.q1-iqr;
 				}
 				const findOutliers = dataset.filter(element => ((element < toAdd.low) || (element > toAdd.high)));
@@ -101,11 +106,14 @@
 
 const box = document.querySelector('#boxplot');
 
+//refreshes plot to reflect most current data in the structure
 function rebuildPlot() {
+	//first remove the boxplots and the points
 	chart.removeSeriesAt(0);
 	chart.removeSeriesAt(0);
 
-	series = chart.box(box.data); //box 1
+	//builds all boxplots first
+	series = chart.box(box.data); //box.data is a list of dictionaries
 	series.normal().fill("#0077ff", 0.6);
 	series.hovered().fill("#0077ff", 0.2);
 	series.selected().fill("#0077ff", 0.8);
@@ -117,9 +125,15 @@ function rebuildPlot() {
 
 	series.xPointPosition(0.5);
 
-	series2 = chart.box([
-		{x: "Sample Data", low: 0, q1:0, median:0, q3:0, high:0, outliers: box.points[0]}
-	]); //pts 1
+	//adds points corresponding to boxplots
+	//adds each subarray of points to arr
+	let arr = [];
+	for(let i = 0; i < box.points.length; i++) {
+		//takes the x of the corresponding boxplot so that they line up
+		arr.push({x: box.data[i].x, low:0, q1:0, median:0, q3:0, high:0, outliers: box.points[i]});
+	}
+	//adds arr to the chart
+	series2 = chart.box(arr); //pts 1
 
 	series2.normal().fill("#ff0000", 0.6);
 	series2.normal().stroke({thickness:0});
@@ -147,9 +161,14 @@ anychart.onDocumentReady(function () {
 
 	series.xPointPosition(0.5);
 
-	series2 = chart.box([
-		{x: "Sample Data", low: 0, q1:0, median:0, q3:0, high:0, outliers: box.points[0]}
-	]);
+	//adds each subarray of points to arr
+	let arr = [];
+	for(let i = 0; i < box.points.length; i++) {
+		//takes the x of the corresponding boxplot so that they line up
+		arr.push({x: box.data[i].x, low:0, q1:0, median:0, q3:0, high:0, outliers: box.points[i]});
+	}
+	//adds arr to the chart
+	series2 = chart.box(arr);
 
 	series2.normal().fill("#ff0000", 0.6);
 	series2.normal().stroke({thickness:0});
@@ -189,7 +208,11 @@ function removeData(index) {
 
 //index corresponds to the index of the dropdown menu
 function addPoint(index) {
-	box.points.push(parseInt(document.getElementById("addPoint").value), index);
+	if(box.points[index] == null) {
+		box.points[index] = [parseInt(document.getElementById("addPoint").value)];
+	} else {
+		box.points[index].push(parseInt(document.getElementById("addPoint").value));
+	}
 	rebuildPlot();
 }
 
